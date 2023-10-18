2ba18f43c1edef9fd51d191be8fcc40c59eb95a2
FarrellAbiezaZidan-13519182 <arbiterafarra@gmail.com>
2023-10-18 09:23:30 +0700

    add mod

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, redirect, url_for, request, jsonify
+from flask import send_file
+#from text_to_speech import text_to_speech_process
+app = Flask(__name__)
+
+@app.route('/text-to-speech',methods = ['POST', 'GET'])
+def text_to_speech():
+
+    sentence = request.json['sentence']
+
+    path_to_file = "2.wav" # ganti dengan text_to_speech_process()
+
+    '''return send_file(
+        path_to_file, 
+        mimetype="audio/wav", 
+        as_attachment=True, 
+        download_name="2.wav")'''
+    
+    return jsonify({"response": "Hi " + sentence})
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
